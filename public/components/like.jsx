0620292c3848c707c50108b4e898c2a5182777ba
alonisser <alonisser@gmail.com>
2016-10-10 00:04:01 +0300

    sass example

--- public/components/like.jsx
+++ public/components/like.jsx
@@ -1,14 +1,15 @@
 import React from '../../node_modules/react';
+const likeStyle = require('../scss/like.scss');
 
 export default class LikeButton extends React.Component {
-    
+
     handleClick(event){
         event.preventDefault();
         let isLiked = !this.props.isLiked;
         this.props.onLikeToggle({isLiked:isLiked});
     }
     render() {
-        let heartClass = this.props.isLiked ? 'fa fa-heart' : 'fa fa-heart-o';
+        let heartClass = this.props.isLiked ? 'fa fa-heart like' : 'fa fa-heart-o like';
         return (
 
             <i className={heartClass} aria-hidden="true" onClick={this.handleClick.bind(this)}></i>
